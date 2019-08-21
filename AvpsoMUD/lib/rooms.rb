2b9b39f87bf0ddd5d16ddc6c71f7299b06323bbb
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-08-21 16:44:47 +0100

    Add files via upload

--- AvpsoMUD/lib/rooms.rb
+++ AvpsoMUD/lib/rooms.rb
@@ -1,12 +1,16 @@
 require 'bundler'
 Bundler.require
 
-
+#Connect to Google Drive through API with creds
 session = GoogleDrive::Session.from_service_account_key("../client_secret.json")
 
+#Grabs the spreadsheet from Google by name
 spreadsheet = session.spreadsheet_by_title("Rooms")
 
-worksheet = spreadsheet.worksheets.first
+#Grabs the worksheets by name in the spreadsheet
+worksheet = spreadsheet.worksheet_by_title("Exits")
+room_worksheet = spreadsheet.worksheet_by_title("Rooms")
 
-puts worksheet["B1"]
-
+#test display/POC
+puts worksheet["C3"]
+puts room_worksheet["B2"]
